9ed66bf7d7a4f9d045c6b685deeb27d3d5fd9d54
Divyanshu <divyanshu.20224@knit.ac.in>
2023-10-01 16:56:38 +0530

    Added Searching feature (mongodb regex)

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,10 +6,12 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
@@ -17,8 +19,8 @@ export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get()
-  async findAll(): Promise<Book[]> {
-    return this.bookService.findAll();
+  async findAll(@Query() query: ExpressQuery): Promise<Book[]> {
+    return this.bookService.findAll(query);
   }
 
   @Get(':id')
